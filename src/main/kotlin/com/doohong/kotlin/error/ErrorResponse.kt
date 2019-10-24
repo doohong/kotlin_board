a2946eccfdbd149447b3dffca63bad1913230c1b
doohong <wnghd94@gmail.com>
2019-10-24 18:16:32 +0900

    fieldError

--- src/main/kotlin/com/doohong/kotlin/error/ErrorResponse.kt
+++ src/main/kotlin/com/doohong/kotlin/error/ErrorResponse.kt
@@ -1,2 +1,37 @@
 package com.doohong.kotlin.error
-class ErrorResponse {}
+
+import org.springframework.validation.AbstractBindingResult
+import org.springframework.validation.BindingResult
+import java.util.stream.Collectors
+
+class ErrorResponse(
+        val message: String?,
+        val status: Int?,
+        val errors: List<FieldError>?,
+        val code: String?
+) {
+    inner class FieldError(){
+        private var field: String? = null
+        private var value: String? = null
+        private var reason: String? = null
+
+        private constructor(field: String, value: String, reason: String): this(){
+            this.field = field
+            this.value = value
+            this.reason = reason
+        }
+        fun of(field: String, value: String, reason: String): List<FieldError>{
+            var fieldErrors = ArrayList<FieldError>()
+            var fieldError = FieldError(field, value, reason);
+            fieldErrors.add(fieldError)
+            return fieldErrors
+        }
+        fun of(bindingResult: BindingResult): List<FieldError>{
+            var fieldErrors = bindingResult.getFieldErrors();
+
+            return fieldErrors.stream().map{error -> FieldError(
+                    error.field, error.rejectedValue!!.toString(),error.defaultMessage!!)
+            }.collect(Collectors.toList())
+        }
+    }
+}
